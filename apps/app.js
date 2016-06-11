942a591efed251d364b5e75c6576c5c342c23148
sawheeldon <simon@jampot.design>
2016-06-11 22:39:36 +0100

    Starting

---
+++ apps/app.js
@@ -0,0 +1,16 @@
+$(function(){
+    $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+        showResults(data.Search);
+    });
+});
+
+function showResults(results){
+    var html="";
+    $.each(results, function(index,value){
+        html += '<p>' + value.Title + '</p>'
+        console.log(value.Title);
+    });
+    $('#search-results').html(html);
+}
+
+
